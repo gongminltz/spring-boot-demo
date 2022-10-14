46300ab81bde87528e7a069c980d6a8cc4226f44
unknown <00771487@zoomlion.com>
2022-10-14 09:40:15 +0800

    [demo-helloworld]: SpringBoot helloworld程序

---
+++ SpringBootDemo/demo-helloworld/src/main/java/org/example/Application.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author gongmin
+ * @date 2022/10/14 9:14
+ */
+@SpringBootApplication
+public class Application {
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        logger.info("<main> start");
+
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ SpringBootDemo/demo-helloworld/src/main/java/org/example/controller/TestController.java
@@ -0,0 +1,20 @@
+package org.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 测试
+ *
+ * @author gongmin
+ * @date 2022/10/14 9:18
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @GetMapping("/helloWorld")
+    public String helloWorld() {
+        return "Hello world!";
+    }
+}
