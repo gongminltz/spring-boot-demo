ca6028092bdf83eb12f98434ae8daf529c7b1380
unknown <00771487@zoomlion.com>
2022-10-18 16:01:08 +0800

    [demo-spring-boot-test]: junit测试

---
+++ SpringBootDemo/demo-spring-boot-test/src/test/java/OtherTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+/**
+ * junit测试
+ *
+ * @author gongmin
+ * @date 2022/10/18 16:00
+ */
+public class OtherTest {
+    @Test
+    public void test() {
+        System.out.println("<test> start");
+    }
+}
