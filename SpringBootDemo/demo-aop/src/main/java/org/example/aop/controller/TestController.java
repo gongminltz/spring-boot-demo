0daf2588a9a6091826d7ed3f1c9608fe6e751d59
unknown <00771487@zoomlion.com>
2022-10-19 10:19:06 +0800

    优化代码

--- SpringBootDemo/demo-aop/src/main/java/org/example/aop/controller/TestController.java
+++ SpringBootDemo/demo-aop/src/main/java/org/example/aop/controller/TestController.java
@@ -1,5 +1,6 @@
 package org.example.aop.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/test")
+@Slf4j
 public class TestController {
     @GetMapping("/test")
     public String test(String name) {
+        log.info("<test> name = " + name);
         return "你好 " + name;
     }
 
